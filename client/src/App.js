5b4cbf7ce07018992bc0d7eb380ab10a64b63506
Samuel Neceda <samuel.neceda@gmail.com>
2023-04-13 15:38:43 +0200

    init client

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Lets build the HackHealth app!
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
